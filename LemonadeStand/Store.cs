add18b6efe949362da77c759ec9fad96755c5dda
gregmanthey <gmanthey03@gmail.com>
2019-09-05 14:03:28 -0500

    Updated Store logic in SellItems to take money and add inventory, added base price for store items, added DisplayStoreItems method which currently calls a UI method that doesn't exist yet.

--- LemonadeStand/Store.cs
+++ LemonadeStand/Store.cs
@@ -7,23 +7,34 @@ namespace LemonadeStand
 {
   public class Store
   {
-    private double[] pricesOfCups;
-    private double[] pricesOfLemons;
-    private double[] pricesOfSugar;
-    private double[] pricesOfIce;
+    private double priceOfCups = .1;
+    private double priceOfLemons = .5;
+    private double priceOfSugar = .3;
+    private double priceOfIce = .05;
 
-    public void SellItems(string item, int amount, Player player)
+    public void DisplayStoreItems()
+    {
+      UI.DisplayStoreInventory();
+    }
+    public void SellItems(string item, ushort amount, Player player)
     {
       switch (item)
       {
         case "cups":
-          //player.inventory.cups += amount;
+          player.inventory.money -= amount * priceOfCups;
+          player.inventory.cups += amount;
           break;
         case "lemons":
+          player.inventory.money -= amount * priceOfLemons;
+          player.inventory.lemons += amount;
           break;
         case "sugar":
+          player.inventory.money -= amount * priceOfSugar;
+          player.inventory.sugar += amount;
           break;
         case "ice":
+          player.inventory.money -= amount * priceOfIce;
+          player.inventory.ice += amount;
           break;
         default:
           break;
