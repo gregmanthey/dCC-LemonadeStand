2ec38acfde32a40e40abe64c78f8758f4c5b91d8
gregmanthey <gmanthey03@gmail.com>
2019-09-08 19:40:49 -0500

    Added base logic to Customer class. Including thirstLevel and isBuying member variables, Constructor, and IsCustomerBuying method.

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -7,5 +7,37 @@ namespace LemonadeStand
 {
   public class Customer
   {
+    public int thirstLevel;
+    public bool isBuying;
+    public Customer(Weather weather)
+    {
+      IsCustomerBuying(weather);
+    }
+
+    public void IsCustomerBuying(Weather weather)
+    {
+      int baseBuyingTemperature = 65;
+      int successModifier = 25;
+      int purchaseLevel = 60;
+
+      if(weather.temperature > baseBuyingTemperature)
+      {
+        thirstLevel += successModifier + weather.temperature - baseBuyingTemperature;
+      }
+
+      if(weather.condition == "sunny")
+      {
+        thirstLevel += successModifier;
+      }
+
+      if(thirstLevel > purchaseLevel)
+      {
+        isBuying = true;
+      }
+      else
+      {
+        isBuying = false;
+      }
+    }
   }
 }
