6d5f508dec9a195d23eff40ac01159bb6f07b616
gregmanthey <gmanthey03@gmail.com>
2019-09-08 21:30:46 -0500

    In Weather class, changed member variables to account for forecast and actual temperatures and conditions. Implemented random conditions in the SetTemperature and SetCondition methods (which each update both the forecasted and actual temperatures and store them in the object).

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -7,8 +7,10 @@ namespace LemonadeStand
 {
   public abstract class Weather
   {
-    public short temperature;
-    public string condition;
+    public int forecastTemperature;
+    public string forecastCondition;
+    public int actualTemperature;
+    public string actualCondition;
 
     public Weather()
     {
@@ -18,12 +20,24 @@ namespace LemonadeStand
 
     private void SetTemperature()
     {
-      temperature = 77;
+      forecastTemperature = Randomness.RandomTemperature();
+      actualTemperature = forecastTemperature + Randomness.RandomInt(-5, 5);
     }
 
     private void SetCondition()
     {
-      condition = "sunny";
+      List<string> conditions = new List<string>() { "immaculately beautiful", "sunny", "partly sunny", "partly cloudy", "cloudy", "rainy", "thunderstormy", "nuclear winter" };
+      int randomIndex = Randomness.RandomInt(0, conditions.Count - 1);
+      forecastCondition = conditions[randomIndex];
+      int randomModifier = Randomness.RandomInt(-1, 1);
+      if(randomIndex + randomModifier < 0 || randomIndex + randomModifier > conditions.Count)
+      {
+        actualCondition = conditions[randomIndex];
+      }
+      else
+      {
+        actualCondition = conditions[randomIndex + randomModifier];
+      }
     }
   }
 }
